87f2ef41b940158450e1e774d1ebb17215f8551c
Twinbee <twinbee@gmail.com>
2021-03-23 09:14:08 -0500

    Add database backup and links (for plex)

---
+++ initialSetup03Backups.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/MythTV/mythtv/master/mythtv/programs/scripts/database/mythconverg_backup.pl
+wget https://raw.githubusercontent.com/MythTV/mythtv/master/mythtv/programs/scripts/database/mythconverg_restore.pl
+chmod a+x mythconverg_backup.pl
+chmod a+x mythconverg_restore.pl
---
+++ initialSetup04MakeLinks.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/MythTV/mythtv/master/mythtv/contrib/user_jobs/mythlink.pl
+chmod a+x mythlink.pl
+/home/matt/mythtvutils/mythlink.pl
