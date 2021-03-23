bce19eac4497c38422e7d9cc16b5a682c78ca6f6
Twinbee <twinbee@gmail.com>
2021-03-23 09:44:11 -0500

    add plex info

--- initialSetup04MakeLinks.sh
+++ initialSetup04MakeLinks.sh
@@ -1,3 +1,10 @@
 wget https://raw.githubusercontent.com/MythTV/mythtv/master/mythtv/contrib/user_jobs/mythlink.pl
 chmod a+x mythlink.pl
-/home/matt/mythtvutils/mythlink.pl
+/home/matt/mythtvutils/mythlink.pl --format %T/%T\ -\ %oY.%om.%od\ -\ %S
+
+#install plex
+echo "wget <latest plex link>"
+echo "sudo dpkg -i <plex.deb>"
+echo "http://hostname:32400/web"
+
+ 
